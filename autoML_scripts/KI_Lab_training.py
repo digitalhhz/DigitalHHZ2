e7dae2064f9dfc2ae31677811249a89bd7b8e9ad
angrit06 <andreas.greiss@web.de>
2022-03-07 15:34:12 +0100

    update training script

--- autoML_scripts/KI_Lab_training.py
+++ autoML_scripts/KI_Lab_training.py
@@ -18,7 +18,6 @@ from sklearn.model_selection import train_test_split
 load_dotenv()
 access_key = os.getenv('access_key')
 secret_key = os.getenv('secret_key')
-
 # load data from minio
 client = Minio("10.0.105.60:9000", access_key=access_key,
                     secret_key=secret_key,
@@ -29,7 +28,6 @@ with open("test.csv", "w") as file:
     print(data.data.decode('ascii'),file=file)
 # save data as dataframe
 df = pd.read_csv("test.csv")
-
 # predictor and target 
 X = df[["illuminance","time","temperature"]]
 y = df["occupancy"]
@@ -44,10 +42,8 @@ model.fit(X_train,y_train)
 # To do extended bivariate data analysis of the data, mljar has `extended_eda` method.
 # This will do the bivariate analysis of features in the dataframe against the passed target feature.
 EDA.extensive_eda(X_train,y_train,save_path="/AutoML_1/")
-
 # save results as dataframe
 df_result = pd.read_csv("./AutoML_1/leaderboard.csv")
-
 # track metric on mlflow
 mlflow.set_tracking_uri("http://10.0.105.60:5000")
 for i in df_result.index:
@@ -58,7 +54,6 @@ for i in df_result.index:
 with mlflow.start_run():    
     mlflow.set_experiment(f'KI_lab_occupancy_{df_result["name"][i]}')
     mlflow.log_metrics({df_result["metric_type"][i]: df_result["metric_value"][i], "train_time": df_result["train_time"][i]})
-
 #save model
 with open("automl_ki_lab_occupancy.pkl", 'wb') as f:
     pickle.dump(model, f)
@@ -68,8 +63,16 @@ if not found:
     client.make_bucket("mlflowbucket")
 else:
     print("bucket already exists!")
-client.fput_object("mlflowbucket","automl_ki_lab_occupancy.pkl","./automl_ki_lab_occupancy.pkl")
-client.fput_object("mlflowbucket","leaderboard_ki_lab_occ.csv","./AutoML_1/leaderboard.csv")
+
+result_model = client.fput_object("mlflowbucket","automl_ki_lab_occupancy.pkl","./automl_ki_lab_occupancy.pkl")
+result_leaderboard = client.fput_object("mlflowbucket","leaderboard_ki_lab_occ.csv","./AutoML_1/leaderboard.csv")
+
+print(
+    f"created {result_model.object_name} object; etag: {result_model.etag}, version-id: {result_model.version_id}"
+    )
+print(
+    f"created {result_leaderboard.object_name} object; etag: {result_leaderboard.etag}, version-id: {result_leaderboard.version_id}"
+    )
 # delete results
 shutil.rmtree("./AutoML_1", ignore_errors=True)
 
