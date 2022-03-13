7ca0d481c51600a4b8e872021b1308f76c18fc1c
SedatYasar <83087143+SedatYasar@users.noreply.github.com>
2022-03-13 12:53:39 +0100

    Update storeData.py

--- evidently-scripts/storeData.py
+++ evidently-scripts/storeData.py
@@ -26,13 +26,13 @@ else:
     print("Bucket 'evidently' already exists")
     
 cmd = client.get_object("evidently","influxQueryLast30Days.sql")
+query= cmd.data.decode('ascii')
 
-print('running: ' + cmd)
-subprocess.call(cmd, shell=True)
 
-f = open( "influxQueryLast" + str(lastDays) + "Days.sql", 'w' )
-f.write( cmd + '\n' )
-f.close()
+print('running: ' + query)
+subprocess.call(query, shell=True)
+
+iris_frame = pd.read_csv("influxData.csv")
 
 # load data from minio
 data = client.fget_object("database","influxDataLast1Days.csv")
@@ -41,5 +41,20 @@ data = client.fget_object("database","influxDataLast1Days.csv")
 with open("test.csv", "w") as file:
     print(data.data.decode('ascii'),file=file)
 # save data as dataframe
-df = pd.read_csv("test.csv")
+current = pd.read_csv("test.csv")
 
+iris_frame['target'] = iris_frame.occupancy
+
+#Target and Data Drift Dashboard
+iris_data_and_target_drift_dashboard = Dashboard(tabs=[DataDriftTab(verbose_level=0), CatTargetDriftTab(verbose_level=0)])
+iris_data_and_target_drift_dashboard.calculate(iris_frame[:current], iris_frame[current:], column_mapping=None)
+iris_data_and_target_drift_dashboard.show()
+
+iris_data_and_target_drift_dashboard.save('index.html')
+# load HTML file to MinIO
+client.fput_object(
+    "evidently", "index.html", "./index.html",
+)
+print(
+    "HTML file is successfully uploaded to bucket 'evidently'."
+)
